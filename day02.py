5fc8c3c1e1bc29d4200b529d4fbac757e6c3f437
Jakub Godula <jtgodula@gmail.com>
2020-12-02 17:04:32 +0100

    Add solution for day 2

---
+++ day02.py
@@ -0,0 +1,34 @@
+def load_input():
+    input_list = []
+    with open('inputs/input2.txt') as file:
+        for line in file:
+            line = line.split()
+            r = map(int, line[0].split('-'))
+            letter = line[1][0]
+            password = line[2]
+            input_list.append((r, letter, password))
+    return input_list
+
+
+def part_1():
+    result = 0
+    for (min_occ, max_occ), letter, password in load_input():
+        occ = sum(1 for c in password if c == letter)
+        if min_occ <= occ <= max_occ:
+            result += 1
+    print(result)
+
+
+def part_2():
+    result = 0
+    for (pos_1, pos_2), letter, password in load_input():
+        char_1 = password[pos_1 - 1]
+        char_2 = password[pos_2 - 1]
+        if char_1 != char_2 and (char_1 == letter or char_2 == letter):
+            result += 1
+    print(result)
+
+
+if __name__ == '__main__':
+    part_1()
+    part_2()
