a518031df93a0ac1254ba7f29c9484c96e17faf3
Jakub Godula <jtgodula@gmail.com>
2020-12-04 23:05:40 +0100

    Add solution for day 3

---
+++ day03.py
@@ -0,0 +1,44 @@
+from functools import reduce
+
+
+def part_1():
+    num_trees = 0
+    x = 0
+
+    with open('inputs/input3.txt') as file:
+        for line in file:
+            if line[x] == '#':
+                num_trees += 1
+            x = (x + 3) % (len(line) - 1)
+
+    print(num_trees)
+
+
+def part_2():
+    moves = [
+        (1, 1),
+        (3, 1),
+        (5, 1),
+        (7, 1),
+        (1, 2),
+    ]
+    xs = [0] * len(moves)
+    results = [0] * len(moves)
+
+    with open('inputs/input3.txt') as file:
+        for y, line in enumerate(file):
+            for i, (x_d, y_d) in enumerate(moves):
+                if y % y_d != 0:
+                    continue
+
+                if line[xs[i]] == '#':
+                    results[i] += 1
+
+                xs[i] = (xs[i] + x_d) % (len(line) - 1)
+
+    print(reduce(lambda x, y: x * y, results))
+
+
+if __name__ == '__main__':
+    part_1()
+    part_2()
